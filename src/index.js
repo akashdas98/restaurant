3adf2bcc1acf6968df22541e8972683e19b75fed
Akash Das <akash42662012@gmail.com>
2019-08-04 05:26:27 +0530

    Start building tabbed browsing logic.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import {pageload} from './pageload';
 
 pageload();
+
+const Tabs = (() => {
+  const tabs = document.querySelectorAll('nav a');
+  
+  function clickTab() {
+    tabs.forEach(tab => {
+      tab.addEventListener('click', () => {
+        reset();
+        newTab(tab);
+      });
+    });
+  }
+
+  function reset() {
+    Array.from(tabs).forEach(tab => {
+      tab.style.borderBottom = '0';
+    });
+  }
+
+  function newTab(tab) {
+    tab.style.borderBottom = '5px solid rgba(211, 110, 15, 0.95)';
+  }
+
+  return {clickTab};
+})();
+
+Tabs.clickTab();
